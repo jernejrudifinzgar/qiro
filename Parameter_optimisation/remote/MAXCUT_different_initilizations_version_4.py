f190dcafc03f3cc15e7ad2358ca21cc33117d8af
Max Passek <Q642074@lmucm1010193.fritz.box>
2023-11-10 23:20:22 +0100

    update correlations

--- Parameter_optimisation/remote/MAXCUT_different_initilizations_version_4.py
+++ Parameter_optimisation/remote/MAXCUT_different_initilizations_version_4.py
@@ -38,7 +38,7 @@ num_runs=1
 num_random=5
 
 colors=['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:pink', 'tab:brown', 'tab:pink', 'tab:grey', 'tab:olive']
-ps=[4]#[1, 2, 3]
+ps=[1, 2, 3]
 
 dictionary_reg = {}
 
