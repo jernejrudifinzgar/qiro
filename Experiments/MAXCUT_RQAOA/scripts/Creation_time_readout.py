83de58f45e9b2ae12bc5a517c10ed84c4952ada5
Max <maxpassek@googlemail.com>
2024-01-15 10:40:22 +0100

    update

--- Experiments/MAXCUT_RQAOA/scripts/Creation_time_readout.py
+++ Experiments/MAXCUT_RQAOA/scripts/Creation_time_readout.py
@@ -5,21 +5,21 @@ def get_change_time(ns, ps, runs, version):
     my_path = os.path.dirname(__file__)
     my_path = os.path.dirname(my_path)
     for n in ns:
-        for p in ps:
-            for run in runs:
+        for run in runs:
+            for p in ps:
                 file_path = my_path + f"/data/results_run_{run}_n_{n}_p_{p}_wo_recalc_version_{version}.pkl"
                 if os.path.exists(file_path):
                     mod_time = os.path.getmtime(file_path)
                     readable_time = time.ctime(mod_time)
 
-                    print(readable_time)
+                    print(f"Run {run}, p {p}: {readable_time}")
 
 
 
 if __name__ == '__main__':
     ns = [50]
-    ps = [1]
-    runs = [0]
+    ps = [1, 2, 3]
+    runs = list(range(10))
     version = 1
 
     get_change_time(ns, ps, runs, version)
