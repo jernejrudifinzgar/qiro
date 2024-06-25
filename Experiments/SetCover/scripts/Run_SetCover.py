bb269b1c89a7e1fe666ad241cceb1643e82adf7e
Max <maxpassek@googlemail.com>
2024-06-25 14:49:02 +0200

    update

--- Experiments/SetCover/scripts/Run_SetCover.py
+++ Experiments/SetCover/scripts/Run_SetCover.py
@@ -172,7 +172,7 @@ def main_3():
             problem = Generator.SetCover(U, V, A=2, B=1)
             #if problem.num_variables<50:
             if len(U) >10 and len(U)<=15:
-                if problem.num_variables <= 55:
+                if problem.num_variables > 55 and problem.num_variables <= 60:
                     try: 
                         with open(f"../data/results_node{n}_run_{run}_idx_{idx}p_2.json", 'r') as f:
                             data = json.loads(f.read())
